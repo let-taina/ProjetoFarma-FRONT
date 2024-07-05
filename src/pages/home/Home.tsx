0aa1ea034c8dfd9d6fb050089c389f737bbeecf4
let-taina <le_taina@hotmail.com>
2024-07-05 14:30:57 -0300

    1parte

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -9,8 +9,8 @@ function Home() {
         <div className="bg-indigo-900 flex justify-center">
           <div className='container grid grid-cols-2 text-white'>
             <div className="flex flex-col gap-4 items-center justify-center py-4">
-              <h2 className='text-5xl font-bold'>Seja bem vindo (a)!</h2>
-              <p className='text-xl'>Aqui colocamos sua saúde em primeiro lugar!</p>
+              <h2 className='text-5xl font-bold'>Seja bem vindo(a)!</h2>
+              <p className='text-xl'>Colocamos sua saúde em primeiro lugar!</p>
               <div className="flex justify-around gap-4">
               <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver produtos</button>
               </div>
